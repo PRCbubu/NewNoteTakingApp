e22969a1df0c39c6d42c97a2978d52285cd5f427
BabuRoyC <prcbubu@gmail.com>
2024-07-11 16:57:26 +0530

    feat: Add NoteViewModelFactory
    This commit introduces a ViewModelFactory for creating instances of NoteViewModel.
    
    - Creates a new class `NoteViewModelFactory` that
     takes an Application and a NoteRepository as dependencies.
    - Implements the `create` method to provide instances of NoteViewModel.

---
+++ app/src/main/java/com/practicing/notetakingapp2/viewModel/NoteViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.practicing.notetakingapp2.viewModel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.practicing.notetakingapp2.repository.NoteRepository
+
+class NoteViewModelFactory(val app: Application, private val noteRepository: NoteRepository): ViewModelProvider.Factory
+{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NoteViewModel(app, noteRepository) as T
+    }
+}
